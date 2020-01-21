8f53190c8ffea2be05c55ee57052e100509b4a42
lda162922@gmail.com <lda162922@gmail.com>
2020-01-22 00:27:53 +0800

    图片拍照选择

--- app/src/main/java/com/xfb/xinfubao/activity/UserInfoActivity.kt
+++ app/src/main/java/com/xfb/xinfubao/activity/UserInfoActivity.kt
@@ -3,10 +3,7 @@ package com.xfb.xinfubao.activity
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
-import android.support.v4.content.FileProvider
 import com.careagle.sdk.callback.PermissionCallBack
 import com.careagle.sdk.utils.Permission
 import com.careagle.sdk.utils.RxPermissionsUtil
@@ -14,10 +11,7 @@ import com.xfb.xinfubao.R
 import com.xfb.xinfubao.dialog.DialogUtils
 import com.xfb.xinfubao.utils.loadLocalUriCircle
 import com.zhihu.matisse.Matisse
-import com.zhihu.matisse.MimeType
-import com.zhihu.matisse.engine.impl.GlideEngine
 import kotlinx.android.synthetic.main.activity_user_info.*
-import java.io.File
 
 
 /** 我的资料 */
@@ -35,43 +29,9 @@ class UserInfoActivity : DefaultActivity() {
     override fun initView(savedInstanceState: Bundle?) {
         myToolbar.setClick { finish() }
         tvHeader.setOnClickListener {
-            RxPermissionsUtil.requestPermission(this, object : PermissionCallBack() {
-                override fun success() {
-                    DialogUtils.showTakePicDialog(this@UserInfoActivity, {
-                        Matisse.from(this@UserInfoActivity)
-                            .choose(MimeType.ofImage())
-                            .countable(true)
-                            .imageEngine(GlideEngine())
-                            .thumbnailScale(0.85f)
-                            .forResult(REQ_CHOICE_FROM_ALBUM)
-                    }) {
-                        // 步骤一：创建存储照片的文件
-                        val path =
-                            "$filesDir${File.separator}images${File.separator}${System.currentTimeMillis()}"
-                        val file = File(path, "test.jpg")
-                        if (!file.getParentFile().exists())
-                            file.getParentFile().mkdirs()
-
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            //步骤二：Android 7.0及以上获取文件 Uri
-                            mUri = FileProvider.getUriForFile(
-                                this@UserInfoActivity,
-                                "com.jsf.piccompresstest",
-                                file
-                            )
-                        } else {
-                            //步骤三：获取文件Uri
-                            mUri = Uri.fromFile(file)
-                        }
-                        //步骤四：调取系统拍照
-                        val intent = Intent("android.media.action.IMAGE_CAPTURE")
-                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
-                        startActivityForResult(intent, REQUEST_TAKE_PHOTO_CODE)
-                    }
-                }
-            }, Permission.WRITE_EXTERNAL_STORAGE, Permission.CAMERA)
-
-
+            DialogUtils.showTakePicDialog(this@UserInfoActivity) {
+                mUri = it
+            }
         }
     }
 
--- app/src/main/java/com/xfb/xinfubao/dialog/DialogUtils.kt
+++ app/src/main/java/com/xfb/xinfubao/dialog/DialogUtils.kt
@@ -1,7 +1,13 @@
 package com.xfb.xinfubao.dialog
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.graphics.drawable.ColorDrawable
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AlertDialog
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -9,7 +15,16 @@ import android.view.WindowManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.careagle.sdk.Config
+import com.careagle.sdk.callback.PermissionCallBack
+import com.careagle.sdk.utils.Permission
+import com.careagle.sdk.utils.RxPermissionsUtil
 import com.xfb.xinfubao.R
+import com.xfb.xinfubao.activity.UserInfoActivity
+import com.zhihu.matisse.Matisse
+import com.zhihu.matisse.MimeType
+import com.zhihu.matisse.engine.impl.GlideEngine
+import java.io.File
 
 
 class DialogUtils {
@@ -63,8 +78,8 @@ class DialogUtils {
             return dialog
         }
 
-        /** 照片 */
-        fun showTakePicDialog(context: Context,selectPic:()->Unit,takePic:()->Unit) {
+        /** 拍照 选择照片 */
+        fun showTakePicDialog(context: Activity, takePic: (uri: Uri) -> Unit) {
             val builder = AlertDialog.Builder(context)
             builder.setCancelable(true)
             val view = LayoutInflater.from(context)
@@ -82,15 +97,49 @@ class DialogUtils {
                 dialog?.dismiss()
             }
             view.findViewById<TextView>(R.id.tvSelectPic).setOnClickListener {
-                selectPic()
-                dialog?.dismiss()
+                RxPermissionsUtil.requestPermission(context, object : PermissionCallBack() {
+                    override fun success() {
+                        Matisse.from(context)
+                            .choose(MimeType.ofImage())
+                            .countable(true)
+                            .imageEngine(GlideEngine())
+                            .thumbnailScale(0.85f)
+                            .forResult(UserInfoActivity.REQ_CHOICE_FROM_ALBUM)
+                        dialog?.dismiss()
+                    }
+                }, Permission.WRITE_EXTERNAL_STORAGE)
             }
             view.findViewById<TextView>(R.id.tvTakePic).setOnClickListener {
-                takePic()
-                dialog?.dismiss()
+                RxPermissionsUtil.requestPermission(context, object : PermissionCallBack() {
+                    override fun success() {
+                        // 步骤一：创建存储照片的文件
+                        val path =
+                            "${Config.getFileCacheDirPath()}${File.separator}images"
+                        val file = File(path, "${System.currentTimeMillis()}.jpg")
+                        if (!file.getParentFile().exists())
+                            file.getParentFile().mkdirs()
+                        val mUri: Uri?
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            //步骤二：Android 7.0及以上获取文件 Uri
+                            mUri =
+                                FileProvider.getUriForFile(context, "com.jsf.piccompresstest", file)
+                        } else {
+                            //步骤三：获取文件Uri
+                            mUri = Uri.fromFile(file)
+                        }
+                        //步骤四：调取系统拍照
+                        val intent = Intent("android.media.action.IMAGE_CAPTURE")
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri)
+                        context.startActivityForResult(
+                            intent,
+                            UserInfoActivity.REQUEST_TAKE_PHOTO_CODE
+                        )
+                        takePic(mUri)
+                        dialog?.dismiss()
+                    }
+                }, Permission.WRITE_EXTERNAL_STORAGE, Permission.CAMERA)
             }
             dialog?.window?.setBackgroundDrawable(dw)
-
         }
     }
 }
