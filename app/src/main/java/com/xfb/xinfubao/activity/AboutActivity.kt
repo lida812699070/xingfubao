9f8307d797af81a70350657833eda8c7b018d4e8
lda162922@gmail.com <lda162922@gmail.com>
2020-01-22 16:55:34 +0800

    关于我们

--- app/src/main/java/com/xfb/xinfubao/activity/AboutActivity.kt
+++ app/src/main/java/com/xfb/xinfubao/activity/AboutActivity.kt
@@ -1,14 +1,19 @@
 package com.xfb.xinfubao.activity
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import com.xfb.xinfubao.BuildConfig
 import com.xfb.xinfubao.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : DefaultActivity() {
     override fun getLayoutId(): Int {
         return R.layout.activity_about
     }
 
+    @SuppressLint("SetTextI18n")
     override fun initView(savedInstanceState: Bundle?) {
-
+        myToolbar.setOnClickListener { finish() }
+        tvVersion.text = "当前版本号:${BuildConfig.VERSION_NAME}"
     }
 }
