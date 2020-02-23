4dbbbf32e2c327b5af4a68d1cf049f992497fd50
lda162922@gmail.com <lda162922@gmail.com>
2020-02-23 13:22:15 +0800

    bug

--- app/src/main/java/com/xfb/xinfubao/activity/InviteFriendActivity.kt
+++ app/src/main/java/com/xfb/xinfubao/activity/InviteFriendActivity.kt
@@ -5,6 +5,7 @@ import com.careagle.sdk.Config
 import com.careagle.sdk.utils.CommentUtils
 import com.careagle.sdk.utils.MyBitmapUtils
 import com.xfb.xinfubao.R
+import com.xfb.xinfubao.constant.Constant.SERVER_API
 import com.xfb.xinfubao.utils.ConfigUtils
 import com.xfb.xinfubao.utils.ZXingUtils
 import kotlinx.android.synthetic.main.activity_invite_friend.*
@@ -13,8 +14,6 @@ import java.io.File
 //https://fenxianghome.oss-cn-hangzhou.aliyuncs.com/apk/testapk/app-fengxiang-debug.apk
 /** 邀请好友 */
 class InviteFriendActivity : DefaultActivity() {
-    var downloadUrl =
-        "https://fenxianghome.oss-cn-hangzhou.aliyuncs.com/apk/testapk/app-fengxiang-debug.apk"
 
     override fun getLayoutId(): Int {
         return R.layout.activity_invite_friend
@@ -22,6 +21,7 @@ class InviteFriendActivity : DefaultActivity() {
 
     override fun initView(savedInstanceState: Bundle?) {
         myToolbar.setClick { finish() }
+        val downloadUrl = SERVER_API + "/help/register/${ConfigUtils.mUserInfo.inviteCode}/1"
         val qrImage = ZXingUtils.createQRImage(downloadUrl, 500, 500)
         ivQrCode.setImageBitmap(qrImage)
         //下载保存
